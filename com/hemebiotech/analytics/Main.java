53ff3474bc7184eb61de589965c367b26a81587d
Guillaume Pot <gzzwctj84g15-student@anonymous-codio.com>
2023-03-15 07:58:50 +0000

    refactor analytics counter

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -4,3 +4,18 @@ import java.util.List;
 import java.util.Map;
 
 // WRITE YOUR CODE HERE
+// public class Main{
+
+//     public final ISymptomReader  reader ;
+//     public final ISymptomWriter writer ;
+//     public final AnalyticsCounter counter;
+
+
+//     public main(ISymptomReader reader, ISymptomWriter writer,AnalyticsCounter counter){
+
+    
+
+//     }
+
+
+// }
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -2,23 +2,27 @@ package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
-
 
 	@Override
 	public void writeSymptoms(Map<String, Integer> symptoms) {
         if(symptoms.isEmpty() != true){
 		    try{
-            FileWriter writer = new FileWriter ("result.out");
-            //Ton problème est ici
-            for (Map.Entry mapentry : symptoms.entrySet()) {
-                // writer.write("symptom: "+mapentry.getKey()
-                // + " | number: " + mapentry.getValue() + "\n") ;
-                System.out.println(mapentry.getKey());
-                //writer.write(String.valueOf(mapentry.getKey())+ "\n");
+           FileWriter writer = new FileWriter ("result.out"); 
+            for (Map.Entry mapentry : symptoms.entrySet()) { //2nd soucis: mapentry ?
+                writer.write(mapentry.getKey() + " : " + mapentry.getValue()+"\n");
+                //writer.newLine(); 
+                /*pour le coup -> Codio est pas vraiment en cause, mais plus le comportement
+                //en général de java sous windows notamment, bien qu'ici ce soit un peu bizarre.
+                //En gros, newline insère ici un retour à la ligne, ce qui est bien, parce que c'est demandé
+                //par contre, il y a deux façons de faire un retour à la ligne en dev : 
+                - \n -> retour à la ligne Simple
+                - \r\n -> retour à la ligne avec un carriage return. 
+                -> en l'occurence, ici le newline insère un carriage return, donc ce n'est malheureusement pas
+                la bonne solution ici. Tu Devras absolument passer avec le \n
+                */
             }
             writer.close();
 
